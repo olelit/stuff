ceebd205221c299667f8916f84e392971a42ac04
Oleg Litasov <o.litasov@imedia.by>
2019-08-27 10:16:27 +0300

    merge sort

--- merge_sort.py
+++ merge_sort.py
@@ -1,21 +1,29 @@
-numbers = [12, 534, 4,5,213,12,4,3,65,65,767,9,4,53,5,232,2,34,5,6,6,56,5,9, 12, 44, 3, 1, 3, 5, 7, 8, 5, 4, 23, 1, 87, 54, 1092]
+import random
+import time
 
 def merge(left, right):
     result = []
 
     while len(left) > 0 and len(right) > 0:
-        result.append(max(left.pop(), right.pop()))
-
-        result.append([item for item in left if len(left) > 0 ])
-        result.append([item for item in right if len(right) > 0])
-
+        left_element = left[0]
+        right_element = right[0]
+        result.append(
+            left.pop(0)
+            if left_element <= right_element 
+            else right.pop(0)
+            )
+    while len(left) > 0:
+        result.append(left.pop(0))
+    while len(right) > 0:
+        result.append(right.pop(0))
     return result
 
 def merge_sort(numbers):
     if len(numbers) <= 1:
         return numbers
-    left = [item for item in numbers[:int(len(numbers)/2)]]
-    right = [item for item in numbers[int((len(numbers)/2)):]]
+    middle = int(len(numbers)/2)
+    left = numbers[:middle]
+    right = numbers[middle:]
 
     left = merge_sort(left)
     right = merge_sort(right)
@@ -23,4 +31,8 @@ def merge_sort(numbers):
 
     return result
 
-print(merge_sort(numbers))
+numbers = [random.randint(0,100000) for _ in range(10000)]
+
+start_time = time.time()
+merge_sort(numbers)
+print(time.time() - start_time)
